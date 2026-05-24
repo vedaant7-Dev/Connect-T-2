60caa5458fa09311c108d0fbfe205f494e153e29
vedant__ <278404454+vedaant7-Dev@users.noreply.github.com>
2026-05-25 01:45:20 +0530

    feat: Add Inter font constants for font family usage in app

---
+++ mobile/constants/Fonts.ts
@@ -0,0 +1,6 @@
+export const INTER_REGULAR = "Inter-Regular";
+export const INTER_MEDIUM = "Inter-Medium";
+export const INTER_SEMIBOLD = "Inter-SemiBold";
+export const INTER_BOLD = "Inter-Bold";
+export const INTER_EXTRABOLD = "Inter-ExtraBold";
+export const INTER_LIGHT = "Inter-Light";
