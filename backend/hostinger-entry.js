71b334d4f3af9ab3b74383c231b03a63838d7006
vedant__ <278404454+vedaant7-Dev@users.noreply.github.com>
2026-06-01 18:28:30 +0530

    Add updated backend entry loading all patches

---
+++ backend/hostinger-entry.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+require("./otpProductionPatch.js");
+require("./connectTProductionPatch.js");
+require("./nagarsevakLoginPatch.js");
+require("./jobPortalAuthPatch.js");
+require("./jobPortalMessagePatch.js");
+require("./jobPortalProfilePatch.js");
+require("./server.js");
