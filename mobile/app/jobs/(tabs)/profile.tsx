ee2250fcb5aae371406c1b917d88ddbc12c417bc
vedaant-07 <se7enfits.07@gmail.com>
2026-07-25 18:32:52 +0530

    Use integrated Job Portal profile language selector

--- mobile/app/jobs/(tabs)/profile.tsx
+++ mobile/app/jobs/(tabs)/profile.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-import ProfileLanguageButton from "@/components/ProfileLanguageButton";
 import LocalizedJobPortalProfileScreen from "@/screens/LocalizedJobPortalProfileScreen";
 
 export default function JobPortalProfileRoute() {
   return (
     <View style={styles.root}>
       <LocalizedJobPortalProfileScreen />
-      <ProfileLanguageButton />
     </View>
   );
 }
