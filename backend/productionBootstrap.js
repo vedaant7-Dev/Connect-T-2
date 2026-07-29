03143c4e0136b6de0fd69ddc8940f57c1ab6b389
vedaant-07 <se7enfits.07@gmail.com>
2026-07-29 23:13:17 +0530

    Load five-item audit log display limit

--- backend/productionBootstrap.js
+++ backend/productionBootstrap.js
@@ -7,6 +7,7 @@
 const PRODUCTION_PATCHES = [
   "./otpProductionPatch.js",
   "./profileSessionHydrationPatch.js",
+  "./roleAuditDisplayLimitPatch.js",
   "./utilityStatusActionsPatch.js",
   "./utilityStatusPatch.js",
   "./nagarsevakWardAssignmentPatch.js",
